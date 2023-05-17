9538bbd9aed7a8c4b352a075b8a24f1bf0e18da2
Rohith Varma Buddaraju <i-rohith.buddaraju@devrev.ai>
2023-05-18 00:45:36 +0530

    Add openai request feature

--- client/main.go
+++ client/main.go
@@ -9,6 +9,10 @@ import (
 	pb "github.com/rohithvarma3000/grpc-server/comms"
 )
 
+const (
+	input = "Hello"
+)
+
 func main() {
 	conn, err := grpc.Dial("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -16,7 +20,7 @@ func main() {
 	}
 	defer conn.Close()
 	client := pb.NewChatServiceClient(conn)
-	reply, err := client.ChatReply(context.Background(), &pb.Chat{Input: "Buddy"})
+	reply, err := client.ChatReply(context.Background(), &pb.Chat{Input: input})
 	if err != nil {
 		panic(err)
 	}
--- server/main.go
+++ server/main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/http"
+	"bytes"
+	"encoding/json"
+	
 
 	"google.golang.org/grpc"
 	pb "github.com/rohithvarma3000/grpc-server/comms"
@@ -11,14 +15,66 @@ import (
 
 const (
 	port = 5000
+	openaiURL = "https://api.openai.com/v1/chat/completions"
 )
+
+type OpenAIResponse struct {
+	ID      string    `json:"id"`
+	Object  string    `json:"object"`
+	Created int       `json:"created"`
+	Choices []Choices `json:"choices"`
+	Usage   Usage     `json:"usage"`
+}
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+type Choices struct {
+	Index        int     `json:"index"`
+	Message      Message `json:"message"`
+	FinishReason string  `json:"finish_reason"`
+}
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
 
 type server struct {
 	pb.UnimplementedChatServiceServer
 }
 
 func (s *server) ChatReply(ctx context.Context, in *pb.Chat) (*pb.Reply, error) {
-	return &pb.Reply{Output: "Hello " + in.Input}, nil
+	bodyString := fmt.Sprintf(`{
+		"model":"gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": "%s"}]
+	}`, in.Input)
+
+	body := []byte(bodyString)
+
+	req, err := http.NewRequest("POST", openaiURL, bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer <YOUR_API_KEY>")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	response := &OpenAIResponse{}
+	err = json.NewDecoder(res.Body).Decode(response)
+	if err != nil {
+		panic(err)
+	}
+
+	return &pb.Reply{Output: response.Choices[0].Message.Content}, nil
 }
 
 
