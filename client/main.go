06c405a110a4ac3ffda929d4537a5da4d687eda5
Rohith Varma Buddaraju <i-rohith.buddaraju@devrev.ai>
2023-05-17 23:42:56 +0530

    Create basic client file

--- client/main.go
+++ client/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-
+	"context"
+	"fmt"
+	
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/rohithvarma3000/grpc-server/comms"
 )
 
 func main() {
-	
+	conn, err := grpc.Dial("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := pb.NewChatServiceClient(conn)
+	reply, err := client.ChatReply(context.Background(), &pb.Chat{Input: "Buddy"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(reply.Output)
+
+
 }
